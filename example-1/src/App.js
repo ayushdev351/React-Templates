0733b74b90f9d67893877dc574324ea5c7878601
Ayush Chauhan <79743704+ayushdev351@users.noreply.github.com>
2022-12-07 16:05:36 +0000

    useMemo example created

--- example-1/src/App.js
+++ example-1/src/App.js
@@ -1,33 +1,51 @@
 import "./App.css";
 
-import {useState, useEffect} from "react"
+import {useState, useMemo} from "react";
 
 function App() {
-  const [user, setUser] = useState("octocat");
-  const [data, setData] = useState({});
+  const [input, setInput] = useState(0);
+  const [counter, setCounter] = useState(0);
 
+  // DOM manipulation function
+  // Called only when corresponding DOM element changes
   const changeHandler = (e) => {
     e.preventDefault();
-    setUser(e.target.value);
+    setInput(e.target.value);
+    console.log("Input function");
   }
 
-  useEffect(() => {
+  // DOM manipulation function
+  // Called only when corresponding DOM element changes
+  const increment = () => {
+    setCounter(counter + 1);
+    console.log("Counter function");
+  }
 
-    fetch(`https://api.github.com/users/${user}`)
-    .then((res) => res.json())
-    .then((data) => setData(data));
+  // Normal Function
+  // Will be recreated on every render
+  // Each render happen when any state change
 
-    console.log("Effected")
-  }, [user])
+  const calculation = (n) => {
+    console.log("Fucking Calculated");
+    return n * n * n;
+  }
+
+  // So we create a memo to memoise the result of the function
+  // And recalculate only when dependecy (input) changes
+
+  const memoisedCalculation = useMemo(() => calculation(input), [input])
 
   return (
     <div className="App">
-      <h1>User name of </h1>
-      <input onChange={changeHandler}></input>
-      <button onClick ={changeHandler}>enter</button>
-      <h1>is {data.name}</h1>
+      <input onChange = {changeHandler} />
+      <h1>Your Input : {input}</h1>
+      <br/>
+      <h1>Calculation Result : {memoisedCalculation}</h1>
+      <br/>
+      <button onClick = {increment}>Click</button>
+      <h1>You clicked {counter} times</h1>
     </div>
-  );
+  )
 }
 
 export default App;
